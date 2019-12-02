435b98f4865396a964942001cae57fafc3703412
Benjamin Komm <mail@bennyk.de>
2019-12-02 09:17:36 +0100

    Bildgroeße Berechner

--- Bildgroeßen Rechner/src/main/BildgroeßenRechner.java
+++ Bildgroeßen Rechner/src/main/BildgroeßenRechner.java
@@ -3,7 +3,11 @@ package main;
 public class BildgroeßenRechner {
 
     public static void main(String[] args) {
-        
+
+        int ergebnis = (1024 * 768) * 24 + 432;
+
+        System.out.println("Byte: " + ergebnis / 8 + "\nBit: " + ergebnis + "\nKByte: " + (ergebnis / 8) / 1000);
+
     }
 
 }
