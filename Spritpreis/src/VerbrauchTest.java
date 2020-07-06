da838503c7d539d7dd74901ed3fcc3353ce0154c
Benjamin Komm <mail@bennyk.de>
2020-07-06 10:07:00 +0200

    wip

--- Spritpreis/src/VerbrauchTest.java
+++ Spritpreis/src/VerbrauchTest.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
 
 public class VerbrauchTest {
 	
 	public static void main(String[] args) {
-		Verbrauch verbrauch = new Verbrauch(20.6, 45.0, 4.5);
+		Scanner scanner = new Scanner(System.in);
+		double strecke = scanner.nextDouble();
+
+		Verbrauch verbrauch = new Verbrauch(strecke, 45.0, 4.5);
 		System.out.println("Verbrauch Pro 100km: " + verbrauch.verbrauchPro100Km());
 	}
 
